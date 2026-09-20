ec719450a1dffa155d3e731d82de8522a3c1396e
Will Kourafas <143365037+Contr0versy@users.noreply.github.com>
2026-09-19 22:09:18 -0400

    Import site extras stylesheet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "./sherpa-theme.css";
+import "./site-extras.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
